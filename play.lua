2816100258ebe03d7ec6ea35fe42164e888d3f85
nissene99 <enissenacc@protonmail.com>
2022-11-13 00:35:59 +0100

    Update play.lua

--- play.lua
+++ play.lua
@@ -5,7 +5,7 @@ local decoder = dfpwm.make_decoder()
 local nft = require("cc.image.nft")
 
 local url_image = "https://raw.githubusercontent.com/nissene99/nggyu-cc-tweaked/main/image/"
-local url_sound = "https://raw.githubusercontent.com/nissene99/nggyu-cc-tweaked/main/audio/small.dfpwm"
+local url_sound = "https://raw.githubusercontent.com/nissene99/nggyu-cc-tweaked/main/audio/"
 
 local function download(url)
     local request = http.get(url)
@@ -18,11 +18,11 @@ end
 local function play(url)
     local data = download(url)
     -- save the file to disk
-    local file = io.open("temp.dfpwm", "wb")
+    local file = io.open("disk/temp.dfpwm", "wb")
     file:write(data)
     file:close()
     -- play the file
-    for chunk in io.lines("temp.dfpwm", 16*1024) do
+    for chunk in io.lines("disk/temp.dfpwm", 16*1024) do
         local buf = decoder(chunk)
 
         while not speaker.playAudio(buf) do
@@ -30,8 +30,10 @@ local function play(url)
         end
     end
     -- delete the file
-    fs.delete("temp.dfpwm")
+    fs.delete("disk/temp.dfpwm")
 end
+
+
 
 local function format_number(num)
     local str = tostring(num)
@@ -39,6 +41,13 @@ local function format_number(num)
         str = "0" .. str
     end
     return str
+end
+
+local function play_audios(begin_index, end_index)
+    for i = begin_index, end_index do
+        local num = string.format("%03d", i)
+        play(url_sound .. num .. ".dfpwm")
+    end
 end
 
 local function show_image(url, duration)
@@ -69,7 +78,7 @@ local function video_player()
             show_images(begin_index, end_index, duration)
         end,
         function()
-            play(url_sound)
+            play_audios(0, 106)
         end
     )
 end
